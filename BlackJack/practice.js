3c8d41a99a601a4e766c1423dc63fcedd568cf29
Theeprime <areriduncan114@gmail.com>
2024-03-15 02:11:07 +0300

    end week begin commit

--- BlackJack/practice.js
+++ BlackJack/practice.js
@@ -1,3 +1,11 @@
 
 
 
+const divCont = document.getElementById("div-cont")
+
+divCont.innerHTML = "<button  onclick='buy()'>Buy!</button>"
+
+function buy(){
+    divCont.innerHTML +="<p>Thank you for buying</p>"
+}
+
